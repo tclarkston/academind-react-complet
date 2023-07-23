bcf1c8c6941a30ad1fdf034e342f313e8f39632e
Todd Clarkston <todd@wireservers.com>
2023-07-23 17:06:57 -0600

    Through ch5 solution

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -4,28 +4,36 @@ import './Expenses.css'
 
 const Expenses = (props) => {
 
-  return (<Card className='expenses'>
-    <ExpenseItem
-      title={props.items[0].title}
-      amount={props.items[0].amount}
-      date={props.items[0].date}>
-    </ExpenseItem>
-    <ExpenseItem
-      title={props.items[1].title}
-      amount={props.items[1].amount}
-      date={props.items[1].date}>
-    </ExpenseItem>
-    <ExpenseItem
-      title={props.items[2].title}
-      amount={props.items[2].amount}
-      date={props.items[2].date}>
-    </ExpenseItem>
-    <ExpenseItem
-      title={props.items[3].title}
-      amount={props.items[3].amount}
-      date={props.items[3].date}>
-    </ExpenseItem>
-  </Card>);
+  const yearChangeHandler = (year) => {
+    console.log('year change handled');
+  };
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}>
+        </ExpenseItem>
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}>
+        </ExpenseItem>
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}>
+        </ExpenseItem>
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}>
+        </ExpenseItem>
+      </Card>
+    </div>
+  );
 };
 
 export default Expenses
