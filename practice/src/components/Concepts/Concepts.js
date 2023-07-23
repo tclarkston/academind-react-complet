49043aa14d7a8b9788949316b2ca9df0b59fa204
Todd Clarkston <todd@wireservers.com>
2023-07-22 19:38:15 -0600

    Adding concepts

---
+++ practice/src/components/Concepts/Concepts.js
@@ -0,0 +1,17 @@
+const Concepts = (props) => {
+  return (
+    <ul id="concepts">
+      <li className="concept">
+        
+      </li>
+      <li className="concept">
+
+      </li>
+      <li className="concept">
+
+      </li>
+    </ul>
+  );
+}
+
+export default Concepts
