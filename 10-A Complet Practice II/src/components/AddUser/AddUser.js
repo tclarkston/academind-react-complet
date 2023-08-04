cf012a214d78c506d3ccfb3cadd8ff432cf377c8
Todd Clarkston <todd@wireservers.com>
2023-08-03 18:58:01 -0600

    half of chapter 10

--- 10-A Complet Practice II/src/components/AddUser/AddUser.js
+++ 10-A Complet Practice II/src/components/AddUser/AddUser.js
@@ -1,10 +1,41 @@
+import React, {useState} from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css'
+import Button from '../UI/Button';
+
 const AddUser = props => {
-
-  const submitHandler = event =>{};
+  const [userName, setUserName] = useState('');
+  const [age, setAge] = useState('');
   
-  <form onSubmit={submitHandler}> 
+  const submitHandler = event => {
+    event.preventDefault();
 
-  </form>
+  };
+
+  const clickHandler = event => {
+    console.log(userName, age)
+
+  };
+  
+  const userNameChangeHandler = event => {
+    setUserName(event.target.value);
+  }
+
+  const ageChangeHandler = event => {
+    setAge(event.target.value);
+  }
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={submitHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' onChange={userNameChangeHandler} />
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type='number' onChange={ageChangeHandler}/>
+        <Button type='submit' onClick={clickHandler}>Add User</Button>
+      </form>
+    </Card>
+  )
 };
 
 export default AddUser;
