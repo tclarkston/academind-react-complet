ebc9b6b70b9b549082b699a4099bfacee9a29478
Todd Clarkston <todd@wireservers.com>
2023-08-08 10:28:49 -0600

    Added state

--- 10-A Complet Practice II/src/App.js
+++ 10-A Complet Practice II/src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
 function App() {
+
+  const [usersList, setUsersList] = useState([]);
+  
   return (
     <div>
       <AddUser />
