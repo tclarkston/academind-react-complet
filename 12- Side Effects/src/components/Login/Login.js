eebcf810cced0c87dc0a3f8c8c9d77ea09bc775a
todd <todd@wireservers.com>
2023-08-11 16:50:13 -0600

    Finished Cleanup of Effects

--- 12- Side Effects/src/components/Login/Login.js
+++ 12- Side Effects/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,12 +11,24 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+
+    const handler = setTimeout(() => {
+      console.log('Checking form validity');
+
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500)
+    
+    return () => {
+      console.log('Cleanup');
+      clearTimeout(handler);
+    }
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
