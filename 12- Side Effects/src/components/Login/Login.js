ea3424f8d649ea9ce6a34c3f80a5050257a16e7e
Todd Clarkston <todd@wireservers.com>
2023-08-13 08:17:59 -0600

    Updated Library

--- 12- Side Effects/src/components/Login/Login.js
+++ 12- Side Effects/src/components/Login/Login.js
@@ -22,7 +22,7 @@ const Login = (props) => {
     }, 500)
     
     return () => {
-      console.log('Cleanup');
+      console.log('CLEANUP');
       clearTimeout(handler);
     }
   }, [enteredEmail, enteredPassword]);
